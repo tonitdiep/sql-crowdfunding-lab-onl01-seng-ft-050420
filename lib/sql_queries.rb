e7fa566689fd7f2a87443d03d496a5db2b46ca42
Toni  Diep <adiep123@gmail.com>
2020-06-10 09:13:31 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -13,6 +13,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
   GROUP BY pledges.project_id, projects.title 
   HAVING projects.id = pledges.project_id 
   ORDER BY projects.title ASC";
+  # "SELECT projects.title, SUM(pledges.amount) FROM projects INNER JOIN pledges ON pledges.project_id = pledges.project_id GROUP BY pledges.project_id, projects.title  HAVING projects.id = pledges.project_id ORDER BY projects.title ASC";
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
